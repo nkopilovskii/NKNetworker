409f293bbada94fee87cd0a1fff6565d73a5dabb
Nick Kopilovskii <nkopilovskii@gmail.com>
2019-07-09 14:15:55 +0300

    NKNResponse

---
+++ NKNetworker/Classes/NKNResponse.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  NKNetworker
+//
+//  Created by Nick Kopilovskii on 7/9/19.
+//
+
+import Foundation
+
+//MARK: - NKNResponse
+public enum NKNResponse<ResponseT> {
+  case success(ResponseT)
+  case failure(Error)
+  case exception(NKNException)
+  
+  @discardableResult
+  func dematerialize() throws -> ResponseT {
+    switch self {
+    case let .success(value): return value
+    case let .failure(error): throw error
+    case let .exception(exeption): throw exeption
+    }
+  }
+}
+//MARK: -
